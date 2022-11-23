27ce4b4662a029806e2455d57454f9489988b6d3
Mintnoii <Mintnoii@users.noreply.github.com>
2022-11-23 16:44:42 +0000

    deploy: 1dd32cb17da4490a22ad930d8339169fb7f413a0

---
+++ assets/tutorial_create.md.c7a6ea04.lean.js
@@ -0,0 +1,1 @@
+import{_ as a,c as e,o as s,d as n}from"./app.b507ab9c.js";const m=JSON.parse('{"title":"\u5FEB\u901F\u521B\u5EFA\u9879\u76EE","description":"","frontmatter":{},"headers":[],"relativePath":"tutorial/create.md"}'),t={name:"tutorial/create.md"},c=n("",5),o=[c];function p(r,l,d,i,_,h){return s(),e("div",null,o)}const v=a(t,[["render",p]]);export{m as __pageData,v as default};
